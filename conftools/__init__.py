6cc081ee7d9e26c9b537e000858da7d6219ab03c
Raphaël Barrois <raphael.barrois@polytechnique.org>
2014-04-13 14:26:44 +0200

    Add __init__

---
+++ conftools/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Polyconseil SAS
+# This code is distributed under the two-clause BSD License.
+
+
+__author__ = "Polyconseil <opensource+conftools@polyconseil.fr>"
+__version__ = '1.0.0'
+
+
