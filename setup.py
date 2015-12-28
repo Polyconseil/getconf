b3eedfebd3037e6523898edcd93aa493fe2be15f
Fabien Bochu <fabien.bochu@polyconseil.fr>
2015-12-28 12:20:59 +0100

    Drop year in copyright statement

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2010-2015 Polyconseil SAS
+# Copyright (c) Polyconseil SAS
 # This software is distributed under the two-clause BSD license.
 from __future__ import unicode_literals
 import io
