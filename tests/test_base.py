b60ec620ad8591476e0c0adeff687abc58c62c57
Stéphane Raimbault <stephane.raimbault@gmail.com>
2016-08-01 15:00:40 +0200

    Ugly tests on exceptions comptatible with Python 2.6

--- tests/test_base.py
+++ tests/test_base.py
@@ -199,14 +199,12 @@ class ConfigGetterTestCase(unittest.TestCase):
         self.assertEqual('54', getter.getstr('test'))
         self.assertEqual(54, getter.getint('test'))
 
-        if sys.version_info > (2, 7):
-            with self.assertRaises(ValueError):
-                getter.getint('wrong')
+        # Ugly test compatible with Python 2.6...
+        self.assertRaises(ValueError, getter.getint, 'wrong')
 
         self.assertEqual(54.0, getter.getfloat('test'))
-        if sys.version_info > (2, 7):
-            with self.assertRaises(ValueError):
-                getter.getfloat('wrong')
+        # Ugly test compatible with Python 2.6...
+        self.assertRaises(ValueError, getter.getfloat, 'wrong')
 
         self.assertEqual(False, getter.getbool('test'))
         self.assertEqual(['54', ], getter.getlist('test'))
